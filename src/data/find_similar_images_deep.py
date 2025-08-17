813532294f6b48c2fff75613c241e0fb4ed37dd9
Trinh-Quoc-Trong <trinhtrong2423@gmail.com>
2025-08-16 18:18:46 -0600

    test

--- src/data/find_similar_images_deep.py
+++ src/data/find_similar_images_deep.py
@@ -13,6 +13,15 @@ def get_device():
     if torch.cuda.is_available():
         print(">>> Đang sử dụng GPU.")
         return torch.device("cuda")
+    print(">>> Đang sử dụng CPU.")
+    return torch.device("cpu")
+
+def get_feature_extractor(device):
+    '''tai model resnet50 da duoc huan luyen va loai bo lop cuoi cung'''
+    # tai model resnet50
+    model = models.resnet50(pretrained=True)
+    # loai bo lop cuoi cung
+    feature_extractor = torch.nn.sequential(*list(model.children())[:-1])
     
 
 
--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,12 @@
 
+import torch 
 
+def check_gpu():
+    if torch.cuda.is_available():
+        print("GPU is available")
+        return torch.device('cuda')
 
-
+print(check_gpu())
 
 
 
